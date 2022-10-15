c334b60350676e61d65c8cf841b905cc6f13e2a5
Stepanova Asya <asenkabsq@gmail.com>
2022-10-15 03:01:21 +0300

    cases for weather reducer

--- src/store/reducers/weatherReducer.js
+++ src/store/reducers/weatherReducer.js
@@ -1,3 +1,7 @@
+import {SET_WEATHER} from '../actions/getWeatherAction'
+import {SET_FORECAST} from '../actions/getForecastAction'
+
+
 const initialState = {
     weather: undefined, 
     forecast: undefined
@@ -5,6 +9,12 @@ const initialState = {
 
 export let weatherReducer =  (state = initialState, action) => {
     switch(action.type){
+        case SET_WEATHER:
+            const {weather} = action;
+            return {...state, weather}
+        case SET_FORECAST:
+            const {forecast} = action;
+            return {...state, forecast}
         default:
             return state;
     }
