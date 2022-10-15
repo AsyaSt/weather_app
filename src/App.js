93204f7cda5dffd5d1bff866e7f8b8d47313298a
Stepanova Asya <asenkabsq@gmail.com>
2022-10-15 13:46:25 +0300

    added Provider , check actions in App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
+import { useEffect } from 'react';
+import { Provider, useDispatch} from 'react-redux';
 import { store } from './store/store';
+import { getForecast } from './store/actions/getForecastAction';
+import { getWeather } from './store/actions/getWeatherAction';
+
+
+store.subscribe(() => console.log(store.getState()));
+
+const AppWrapper = () => {
+  return (
+    <Provider store={store}> 
+      <App />
+    </Provider>
+  )
+}
 
 function App() {
-  store.subscribe(() => console.log(store.getState()));
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getWeather('paris'));
+    dispatch(getForecast('paris'));
+  }, []);
 
   return (
-    <></>
+
+      <></>
+
   );
 }
 
-export default App;
+export default AppWrapper;
