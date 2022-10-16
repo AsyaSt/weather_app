1c48e208cb8e974e8e695092870ca9ae53693baa
Stepanova Asya <asenkabsq@gmail.com>
2022-10-16 13:21:14 +0300

    searchBar component

---
+++ src/components/searchBar/searchBar.js
@@ -0,0 +1,30 @@
+import { faSearch } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { getForecast } from '../../store/actions/getForecastAction';
+import { getWeather } from '../../store/actions/getWeatherAction';
+
+export const SearchBar = () => {
+    const dispatch = useDispatch();
+    const [value, setValue] =  useState('');
+    return(
+        <div className="search">
+            <form action="/" method="get" className="df">
+                <input type="text"
+                    id="header-search"
+                    placeholder="Search for city"
+                    value={value} onChange={e => setValue(e.target.value)}
+                />
+                <button type="submit" onClick={(e) => {
+                        e.preventDefault();
+                        dispatch(getWeather(value));
+                        dispatch(getForecast(value));
+                        setValue('');
+                    }}>
+                    <FontAwesomeIcon icon={faSearch} />
+                </button>
+            </form>
+        </div>
+    )
+};
